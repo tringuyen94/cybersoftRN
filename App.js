fe52e2571a7fee6714bd57748b57d79a5e567ed4
Tri Nguyen <tringuyen@sosoon.local>
2021-05-04 05:28:17 +0700

    Scroll Learning

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import ZaloChat from './src/Buoi6/ZaloChat'
+import ScrollLearning  from "./src/Buoi6/ScrollLearning"
 
 export default class 
  extends Component {
   render() {
     return (
       <Fragment>
-        <ZaloChat/>
+        < ScrollLearning/>
+        {/* <ZaloChat/> */}
       </Fragment>
     )
   }
